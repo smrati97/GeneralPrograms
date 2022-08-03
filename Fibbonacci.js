9a6caae3dd66070b267ce0cb2b9c923373ff5858
smrati97 <56163272+smrati97@users.noreply.github.com>
2022-08-04 00:55:02 +0530

    Fibbonacci

---
+++ Fibbonacci.js
@@ -0,0 +1,24 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Welcome to Programiz!");
+function Prime(num){
+     let a=0
+     let b=1
+     console.log(a);
+     console.log(b);
+for(let i=1;i<num;i++){
+   
+    
+    let temp = a+b;
+    a=b;
+    b=temp;
+    
+    
+    
+    console.log(temp);
+}
+}
+
+Prime(10);
+
